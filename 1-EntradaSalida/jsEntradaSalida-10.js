62d0a4fea90673b51fa94c921adaaaffd91b25b2
Jessica <36174668+Jessicanbalbuena@users.noreply.github.com>
2018-02-08 22:21:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,15 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
-{
+{var importe
+var descuento
+var importeFinal
+
+importe = parseInt(document.getElementById("importe").value);
+descuento = importe * 0.25;
+importeFinal = importe - descuento;
+
+document.getElementById("resultado").value = importeFinal;
+
 	
 }
