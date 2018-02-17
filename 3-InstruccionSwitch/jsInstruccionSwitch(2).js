4e96cddbb17a18485a42159a7193b5020e0888f0
Jessica <36174668+Jessicanbalbuena@users.noreply.github.com>
2018-02-17 12:50:48 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -7,7 +7,27 @@ alert(mesDelAño)
 
 switch(mesDelAño)
 
-{     
+{case"Enero":
+case"Febrero":
+case "Marzo":
+case "Abril":
+case"Mayo":
+case"Junio":
+alert("Falta poco para el invierno");
+break;
+case"Julio":
+case"Agosto":
+alert("Abrigate que hace frio");
+break;
+case"Septiembre":
+alert("Ya pasamos elfrio, ahora el calor");
+break;
+case"Octubre":
+case"Noviembre":
+case"Diciembre":
+ }
+
+
 
 
 }//FIN DE LA FUNCIÓN
