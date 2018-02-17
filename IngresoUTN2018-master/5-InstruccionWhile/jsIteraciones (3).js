77fc32cecfe64d3115cc64061afdc87154b25a09
Jessica <36174668+Jessicanbalbuena@users.noreply.github.com>
2018-02-17 12:39:06 -0300

    Add files via upload

--- IngresoUTN2018-master/5-InstruccionWhile/jsIteraciones (3).js
+++ IngresoUTN2018-master/5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,17 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while(clave != "utn750")
+
+{clave = prompt("error. La clave es incorrecta, por favor ingrese la clave nuevamente");}
+
+alert("Clave correcta"); 
+
+
 
 }//FIN DE LA FUNCIÓN
+while( !(numero = 0 <= 9 ))
+
+{numero = prompt("Numero incorrecto. Reingrese el numero."); }
+
+alert("Contraseña correcta");
--- IngresoUTN2018-master/5-InstruccionWhile/jsIteraciones (4).js
+++ IngresoUTN2018-master/5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,12 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+while( ! (numero = 0 <= 9) )
+{ 
+	numero = prompt("Numero incorrecto. Ingrese la clave nuevamente.");
 
+}
+
+alert("Contraseña correcta");
 
 }//FIN DE LA FUNCIÓN
