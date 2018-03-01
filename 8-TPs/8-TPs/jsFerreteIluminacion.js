84f4f3d3945a385f3b9b422cbfccc3365b203de2
Jessica <36174668+Jessicanbalbuena@users.noreply.github.com>
2018-03-01 20:01:23 -0300

    Add files via upload

---
+++ 8-TPs/8-TPs/jsFerreteIluminacion.js
@@ -0,0 +1,84 @@
+/*4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+ */
+function CalcularPrecio () 
+{
+     var cantidad;
+     var marca;
+     var precioConDescuento;
+     var descuento;
+     var importeFinal;
+     var IIBB; 
+     var precio = 35;
+     cantidad = parseInt(document.getElementById("Cantidad").value);
+     
+     marca = document.getElementById("Marca").value;
+
+     switch(cantidad) 
+     { 
+         case 1:
+         case 2:  
+         descuento = 0;
+         break;
+        
+    
+         case 3:
+         if(marca == "Argentina Luz" )
+         {
+             descuento =.15;
+         }
+         else if ( marca == "Felipe Lamparas")
+        { 
+            descuento =.10;
+         }
+         else{
+             descuento =.05;
+         }
+        case 4:
+        if(marca = "Argentina Luz" || marca == "Felipe Lamparas")
+        {
+         descuento =.25;   
+         }
+         else 
+         { 
+             descuento =.20;
+         }
+         break; 
+         case 5:
+         if ( marca == "Argentina Luz")
+         {
+             descuento = .40;
+         }        
+        else {
+            descuento = .30;
+        }
+        break;
+
+        default:
+        descuento =.50;
+        break; 
+        }
+
+        precioConDescuento = precio - precio * descuento;
+        
+
+        document.getElementById("precioDescuento").value = precioConDescuento; 
+
+        importeFinal = precioConDescuento * cantidad;
+
+        if(importeFinal > 120)
+        { 
+            IIBB = importeFinal * .10;
+            importeFinal = importeFinal + IIBB;
+
+            alert("El importe a pagar es $ " + importeFinal + " del cual $ " + IIBB + " corresponde al ingreso bruto");
+        }
+
+ }
